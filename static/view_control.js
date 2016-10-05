da17f8a8af04071da30d25b6c22441033f2e3d3e
Miklos Csepella <csepella.miki@gmail.com>
2016-10-05 20:10:54 +0200

    new font, whitespace, minor styling

--- static/view_control.js
+++ static/view_control.js
@@ -5,9 +5,12 @@ $(document).ready(function() {
     var $mkBoardCard = function(title) {
         $('input#newBoardName').val('');
         $(".main-content").append("<div class='col-xs-12 col-sm-6 col-md-3' id='" + title + "'></div>");
-        $("#" + title).append("<div class='panel panel-default list' id='list_1'><div class='panel-heading'><h4>" + title + "</h4></div></div>")
+        $("#" + title).append("<div class='panel panel-default board' id='board_1'><div class='panel-body'><h4>" + title + "</h4></div></div>")
     };
     $(".main-content").empty();
+    $("button").mouseup(function(){
+    // $("button").blur();
+})
     $('#newBoardModal').on('shown.bs.modal', function () {
             $('#newBoardName').focus();
     });
