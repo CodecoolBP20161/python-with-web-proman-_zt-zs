9449503eea1932d1b8a8165d533863644795abf1
Miklos Csepella <csepella.miki@gmail.com>
2016-10-05 15:21:37 +0200

    modal to give title to new boards, jquery clears page and adds boards by add btn, some reforatting

--- static/view_control.js
+++ static/view_control.js
@@ -1,3 +1,14 @@
 /**
  * Created by miki on 2016.10.05..
  */
+$(document).ready(function() {
+    var $mkBoardCard = function(title) {
+        $(".main-content").append("<div class='col-xs-12 col-sm-6 col-md-3' id='" + title + "'></div>");
+        $("#" + title).append("<div class='panel panel-default list' id='list_1'><div class='panel-heading'><h4>" + title + "</h4></div></div>")
+    };
+    $(".main-content").empty();
+    $("#board-heading").empty();
+    $(".modal-footer>.btn").click(function() {
+        $mkBoardCard($("#newBoardModal #newBoardName").val())
+    });
+});
