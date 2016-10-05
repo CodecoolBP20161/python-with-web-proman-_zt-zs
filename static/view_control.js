5e573efe5eb48ebf9b37bba9ddf5519c5c33eaae
Miklos Csepella <csepella.miki@gmail.com>
2016-10-05 15:32:48 +0200

    clear prev value from new board title input field

--- static/view_control.js
+++ static/view_control.js
@@ -3,6 +3,7 @@
  */
 $(document).ready(function() {
     var $mkBoardCard = function(title) {
+        $('#newBoardName').val('');
         $(".main-content").append("<div class='col-xs-12 col-sm-6 col-md-3' id='" + title + "'></div>");
         $("#" + title).append("<div class='panel panel-default list' id='list_1'><div class='panel-heading'><h4>" + title + "</h4></div></div>")
     };
