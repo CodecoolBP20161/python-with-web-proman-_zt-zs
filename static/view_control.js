e1999cdb1dc777b182339b9c051acbfb171c61a4
Miklos Csepella <csepella.miki@gmail.com>
2016-10-05 18:35:17 +0200

    new board input field now on autofocus, enter works for submit

--- static/view_control.js
+++ static/view_control.js
@@ -3,13 +3,23 @@
  */
 $(document).ready(function() {
     var $mkBoardCard = function(title) {
-        $('#newBoardName').val('');
+        $('input#newBoardName').val('');
         $(".main-content").append("<div class='col-xs-12 col-sm-6 col-md-3' id='" + title + "'></div>");
         $("#" + title).append("<div class='panel panel-default list' id='list_1'><div class='panel-heading'><h4>" + title + "</h4></div></div>")
     };
     $(".main-content").empty();
-    $("#board-heading").empty();
+    $('#newBoardModal').on('shown.bs.modal', function () {
+            $('#newBoardName').focus();
+    });
+    // $("#board-heading").empty();
     $(".modal-footer>.btn").click(function() {
-        $mkBoardCard($("#newBoardModal #newBoardName").val())
+        $mkBoardCard($("#newBoardModal #newBoardName").val());
+    });
+    $('input#newBoardName').keypress(function (e) {
+        var key = e.which;
+        if(key == 13) {
+            $mkBoardCard($("#newBoardModal #newBoardName").val());
+            $('#newBoardModal').modal('hide');
+        };
     });
 });
