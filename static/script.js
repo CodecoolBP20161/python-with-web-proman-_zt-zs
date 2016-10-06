9b51fe01625b62d1294c65ff0d311bf10bc3b6cd
cickib <bellakocsis06@gmail.com>
2016-10-06 17:49:39 +0200

    cards style added

--- static/script.js
+++ static/script.js
@@ -1,20 +1,3 @@
-function storageSwitch(storage) {
-    this.saveBoard = function (board) {
-        storage.saveBoard(board);
-    };
-    this.getBoards = storage.getBoards();
-    this.saveCard = function (card) {
-        storage.saveCard(card);
-    };
-    this.getCardsByBoardId = function (board) {
-        storage.getCardsByBoardId(board);
-    };
-};
-
-var localImplementation = new localStore();
-var centralStore = new storageSwitch(localImplementation);
-// now it's possible to use centralStore (storageSwitch instance) instead of a localStore instance
-
 function Board(title) {
     this.title = title;
     if (localStorage.id) {
