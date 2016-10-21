e886b120b8a7957c4e5b3e7ce227fd2e3ddf8963
György Hamar <gyurinak@gmail.com>
2016-10-21 10:10:15 +0200

    pycharm formatting override

--- static/scripts/api_script.js
+++ static/scripts/api_script.js
@@ -6,11 +6,11 @@ $(document).ready(function () {
         $(this).removeClass("add-active");
     });
     $('#add-modal').on('shown.bs.modal', function () {
-        $('#title').focus()
-    })
+        $('#title').focus();
+    });
     $('#add-modal').on('hidden.bs.modal', function () {
         $("#title").val("");
-    })
+    });
     $('.container-main').on('mouseenter', '.btn-board', function () {
         $(this).addClass("active");
     });
@@ -18,21 +18,17 @@ $(document).ready(function () {
         $(this).removeClass("active");
     });
     $('#display-modal').on('shown.bs.modal', function (event) {
-        $('#text').focus()
-        $('.board-modal-title').text($(event.relatedTarget).data("title"))
-        var boardId = $(event.relatedTarget).data("id")
-        $('#card-saver').attr("board-id", boardId)
-        globalImplementation.getCardsByBoardId(boardId)
-
-    })
+        $('#text').focus();
+        $('.board-modal-title').text($(event.relatedTarget).data("title"));
+        var boardId = $(event.relatedTarget).data("id");
+        $('#card-saver').attr("board-id", boardId);
+        globalImplementation.getCardsByBoardId(boardId);
+    });
     $('#display-modal').on('hidden.bs.modal', function () {
         $(".board-modal-title").text("");
-        $('#card-saver').attr("board-id", "")
-        $(".list-group-item").remove()
-        $(".list-group-item-text").remove()
-        $("#text").empty()
-    })
-
-})
-;
-
+        $('#card-saver').attr("board-id", "");
+        $(".list-group-item").remove();
+        $(".list-group-item-text").remove();
+        $("#text").empty();
+    });
+});
