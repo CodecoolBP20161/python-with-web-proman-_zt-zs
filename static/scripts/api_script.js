348f14aa9b9e87b20f76f3fdf4321debe3a26052
György Hamar <gyurinak@gmail.com>
2016-10-21 12:10:16 +0200

    updated scripts with better formatting

--- static/scripts/api_script.js
+++ static/scripts/api_script.js
@@ -1,41 +1,22 @@
 $(document).ready(function () {
-    $("button").on('mouseup', function () {
-        $(this).blur();
-    });
-    $('.container-main').on('mouseenter', '.btn-add', function () {
-        $(this).addClass("add-active");
-    });
-    $(".container-main").on('mouseleave', '.btn-add', function () {
-        $(this).removeClass("add-active");
-    });
     $('#add-modal').on('shown.bs.modal', function () {
-        $('#title').focus()
+        $('#title').focus();
     });
     $('#add-modal').on('hidden.bs.modal', function () {
         $("#title").val("");
-    });
-    $('.container-main').on('mouseenter', '.btn-board', function () {
-        $(this).addClass("active");
-    });
-    $(".container-main").on('mouseleave', '.btn-board', function () {
-        $(this).removeClass("active");
     });
     $('#display-modal').on('shown.bs.modal', function (event) {
         $('#text').focus();
         $('.board-modal-title').text($(event.relatedTarget).data("title"));
         var boardId = $(event.relatedTarget).data("id");
         $('#card-saver').attr("board-id", boardId);
-        globalImplementation.getCardsByBoardId(boardId)
-
+        globalImplementation.getCardsByBoardId(boardId);
     });
     $('#display-modal').on('hidden.bs.modal', function () {
         $(".board-modal-title").text("");
         $('#card-saver').attr("board-id", "");
         $(".list-group-item").remove();
         $(".list-group-item-text").remove();
-        $("#text").empty()
-    })
-
-})
-;
-
+        $("#text").empty();
+    });
+});
--- static/scripts/apistore.js
+++ static/scripts/apistore.js
@@ -23,101 +23,99 @@ function apiStore() {
                         data: JSON.stringify({"title": title}),
                         contentType: 'application/json; charset=utf-8',
                         success: confirm("Board successfully saved.")
-                    })
+                    });
                     $.getJSON('/api/boards', function (board) {
-                        index = board.length
-                        var board = new Board(title, index)
-                        display(board)
-                    })
+                        index = board.length;
+                        var board = new Board(title, index);
+                        display(board);
+                    });
 
-                }
-                else {
-                    alert("The title cannot be empty!")
-                }
-            })
+                } else {
+                    alert("The title cannot be empty!");
+                };
+            });
             $("#title").keypress(function (e) {
                 if (e.which == 13) {
                     e.preventDefault();
-                    $("#save-btn").click()
-                }
-            })
-        })
+                    $("#save-btn").click();
+                };
+            });
+        });
     };
+
     this.getBoards = function () {
         $(document).ready(function () {
             $.getJSON('/api/boards', function (board) {
                 for (var d = 0; d < board.length; d++) {
-                    display(board[d])
-                }
-            })
+                    display(board[d]);
+                };
+            });
         });
     };
+
     this.getCardsByBoardId = function (board) {
         $(document).ready(function () {
-            var route = '/api/cards/' + String(board)
+            var route = '/api/cards/' + String(board);
             $.getJSON(route, function (card) {
                 try {
                     if (card.length > 0) {
                         for (var c = 0; c < card.length; c++) {
                             displayCards(card[c])
-                        }
-                    }
-                }
-                catch (e) {
-                }
-            })
+                        };
+                    };
+                }catch (e) {};
+            });
         });
     };
+
     this.saveCard = function () {
-        var index
+        var index;
         $(document).ready(function () {
-                $("#card-saver").click(function (event) {
-                    event.preventDefault();
-                        var text = $(":input[id=text]").val();
-                        var board = $(this).attr("board-id")
-                        var route = '/api/cards/' + String(board)
-                        if (text.length > 0) {
-                            $.ajax({
-                                method: "POST",
-                                url: "/create_card",
-                                dataType: "json",
-                                data: JSON.stringify({"text": text, "board": board}),
-                                contentType: 'application/json; charset=utf-8',
-                            })
-                            $.getJSON(route, function (card) {
-                                index = card.length + 1
-                            })
-                            var card = new Card(text, index, board)
-                            displayCards(card)
-                        }
-                        else {
-                            alert("The text cannot be empty. Please enter something!")
-                        }
-                    }
-                )
-                $("#text").keypress(function (e) {
-                    if (e.which == 13) {
-                        e.preventDefault();
-                        $("#card-saver").click()
-                    }
-                })
-            }
-        )
+            $("#card-saver").click(function (event) {
+                event.preventDefault();
+                var text = $(":input[id=text]").val();
+                var board = $(this).attr("board-id");
+                var route = '/api/cards/' + String(board);
+                if (text.length > 0) {
+                    $.ajax({
+                        method: "POST",
+                        url: "/create_card",
+                        dataType: "json",
+                        data: JSON.stringify({"text": text, "board": board}),
+                        contentType: 'application/json; charset=utf-8',
+                    });
+                    $.getJSON(route, function (card) {
+                        index = card.length + 1;
+                    });
+                    var card = new Card(text, index, board);
+                    displayCards(card);
+                } else {
+                    alert("The text cannot be empty. Please enter something!");
+                };
+            });
+            $("#text").keypress(function (e) {
+                if (e.which == 13) {
+                    e.preventDefault();
+                    $("#card-saver").click();
+                };
+            });
+        });
     };
-}
+};
 
 var display = function (board) {
     $(".boards-display").after('<div class="container-box col-md-3 col-sm-6"><button type="button" class="btn-board" ' +
-        'data-toggle="modal" data-target="#display-modal" data-title="' + board.title + '" data-id="' + board.id + '">' + board.title + '</button></div>')
-}
+        'data-toggle="modal" data-target="#display-modal" data-title="' + board.title + '" data-id="' + board.id + '\
+        ">' + board.title + '</button></div>');
+};
 
 var displayCards = function (card) {
     $(":input[id=text]").val("")
-    $(".list-group").after('<span class="list-group-item"><p class="list-group-item-text cards" id="card_' + card.id + '">' + card.text + '</p></span>')
-}
+    $(".list-group").after('<span class="list-group-item"><p class="list-group-item-text cards" id="card_\
+    ' + card.id + '">' + card.text + '</p></span>');
+};
 
 var globalImplementation = new apiStore();
-globalImplementation.saveBoard()
-globalImplementation.getBoards()
-globalImplementation.saveCard()
-
+globalImplementation.saveBoard();
+globalImplementation.getBoards();
+globalImplementation.saveCard();
