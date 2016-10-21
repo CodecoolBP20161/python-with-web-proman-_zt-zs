d795ebdf518a6dfd5e04374d88ea9ccd1652fe67
Miklos Csepella <csepella.miki@gmail.com>
2016-10-21 11:52:16 +0200

    btn click animation

--- static/scripts/api_script.js
+++ static/scripts/api_script.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+    $("button").on('mouseup', function () {
+        $(this).blur();
+    });
     $('.container-main').on('mouseenter', '.btn-add', function () {
         $(this).addClass("add-active");
     });
@@ -7,10 +10,10 @@ $(document).ready(function () {
     });
     $('#add-modal').on('shown.bs.modal', function () {
         $('#title').focus()
-    })
+    });
     $('#add-modal').on('hidden.bs.modal', function () {
         $("#title").val("");
-    })
+    });
     $('.container-main').on('mouseenter', '.btn-board', function () {
         $(this).addClass("active");
     });
@@ -18,18 +21,18 @@ $(document).ready(function () {
         $(this).removeClass("active");
     });
     $('#display-modal').on('shown.bs.modal', function (event) {
-        $('#text').focus()
-        $('.board-modal-title').text($(event.relatedTarget).data("title"))
-        var boardId = $(event.relatedTarget).data("id")
-        $('#card-saver').attr("board-id", boardId)
+        $('#text').focus();
+        $('.board-modal-title').text($(event.relatedTarget).data("title"));
+        var boardId = $(event.relatedTarget).data("id");
+        $('#card-saver').attr("board-id", boardId);
         globalImplementation.getCardsByBoardId(boardId)
 
-    })
+    });
     $('#display-modal').on('hidden.bs.modal', function () {
         $(".board-modal-title").text("");
-        $('#card-saver').attr("board-id", "")
-        $(".list-group-item").remove()
-        $(".list-group-item-text").remove()
+        $('#card-saver').attr("board-id", "");
+        $(".list-group-item").remove();
+        $(".list-group-item-text").remove();
         $("#text").empty()
     })
 
